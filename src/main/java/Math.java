48f179e6ca82eb27ead3a2e1a88176ad87490829
XiaoHiei <minhdesign99@gmail.com>
2024-05-10 21:54:32 +0700

    lesson-04

---
+++ src/main/java/Math.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Math {
+    void showPI() {
+        System.out.println(3.1415);
+    }
+
+    int max(int a, int b) {
+        return a > b ? a : b;
+
+    }
+
+    int lode() {
+        Random random = new Random();
+        return random.nextInt(100);
+    }
+}
